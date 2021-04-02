9d41f004b2b07d994ce000961f224208314b1180
mohibuslc <mohib_uslc@outlook.com>
2021-04-03 00:15:27 +0100

    flag+border+addButtom

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-import React,{useState,useEffect} from 'react';
-function App() {
+  import logo from './logo.svg';
+  import './App.css';
+  import React,{useState,useEffect} from 'react';
+  import Country from './Components/Country/Country.js';
 
-  const [count , setCount]= useState([]);
+  function App() {
 
-  useEffect(()=>{
+    const [count , setCount]= useState([]);
+
+    useEffect(()=>{
+        
+        
+        fetch('https://restcountries.eu/rest/v2/all')
+        .then(res => res.json())
+        .then(data =>{ 
+
+          setCount(data);
+
+          console.log(data);
+          const names = data.map(count => count.name)
+
+          console.log(names);
+        })
+      .catch(error => console.log(error))
       
       
-      fetch('https://restcountries.eu/rest/v2/all')
-      .then(res => res.json())
-      .then(data => setCount(data));
-      
-  },[])
-  return (
-    <div className="App">
-      <h1>Countries Info : {count.length}</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    },[])
 
-export default App;
+    const handle = () => {console.log('Country-Added')}; // add button for "eventhandler" :
+
+
+    return (
+      <div className="App">
+
+        
+        <h1>Countries Info : {count.length}</h1>
+        <ul>
+
+              {
+
+                count.map( count => <Country count={count} handle = {handle}> </Country> )
+              }
+
+              </ul>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        
+
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
+  export default App;
---
+++ src/Components/Country/Country.js
@@ -0,0 +1,34 @@
+    import React from 'react';
+
+    const Country = (props) => {
+        
+    const {name , population,flag}=props.count // props : use to access data from API and Show UI: 
+
+
+
+    const flagstyle = {height:'50px'}
+    const countrystyle ={border:'1px solid red', margin:'10px', padding:'10px'}
+
+    const handle = props.handle; // handle use from drieerent file from : " props" ;
+
+
+
+        return ( 
+            <div style={countrystyle}>
+
+
+                <h1>This is Country: {name}</h1>
+
+
+                <img style ={flagstyle} src={flag} alt=""/>
+
+
+                <p> Country Population : {population}</p>
+
+                
+                <button onClick={handle}>Add-Country</button>
+            </div>
+        );
+    };
+
+    export default Country;
