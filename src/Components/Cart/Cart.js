6453329c27ad2d215780068e01ad216c651d4e1c
mohibuslc <mohib_uslc@outlook.com>
2021-04-04 21:58:54 +0100

    cart add

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 const Cart = (props) => {
 
-    const cart = props.cart
+    const cart = props.cart;
+
+    let totalPopulation = 0; // this function used for : add cart with + population: 
+    for (let i = 0; i< cart.length; i++) {
+        const country = cart[i];
+        
+    totalPopulation = totalPopulation+country.population; 
+    }
+    
+
+
     return (
         <div>
             <h2>This Is Cart:{cart.length}</h2>
+            <p>Population : {totalPopulation}</p>
+
         </div>
     );
 };
